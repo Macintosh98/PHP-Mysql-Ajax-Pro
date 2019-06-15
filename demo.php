1d67c6d6273918d58742ae6efd01c86dd5ae0602
Macintosh98 <zambareabhishek@gmail.com>
2019-06-15 10:04:43 +0530

    Moving to jQuery

---
+++ demo.php
@@ -0,0 +1,7 @@
+<?php
+    if ( isset( $_GET['ID'] ) ) 
+    {
+        $ID=$_GET['ID'];
+        echo $ID;
+    }
+?>
--- view.php
+++ view.php
@@ -33,7 +33,7 @@
         </script>
         <div class="container" id="txt"> 
             <br><br><h1>Employees From Policy Planner</h1><p>deatails of all employees:</p><br><br>            
-            <table class="table table-dark">
+            <table class="table table-dark" id="view">
                 <thead>
                 <tr>
                     <th>ID</th>
@@ -81,18 +81,18 @@
                         while($row = $result->fetch_assoc()) {
                             //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
                             ?>
-                            <tr>
+                            <tr id="<?php echo $row['ID']; ?>">
                                 <td><?php echo $row['ID']; ?></td>
-                                <td><?php echo $row['E_name']; ?></td>
-                                <td><?php echo $row['E_mob']; ?></td>
-                                <td><?php echo $row['E_email']; ?></td>
-                                <td><?php echo $row['E_pan']; ?></td>
-                                <td><?php echo $row['E_sal']; ?></td>
-                                <td><?php echo $row['E_Jdate']; ?></td>
-                                <td><?php echo $row['E_status']; ?></td>
+                                <td id='N' ><?php echo $row['E_name']; ?></td>
+                                <td id='M' ><?php echo $row['E_mob']; ?></td>
+                                <td id='E' ><?php echo $row['E_email']; ?></td>
+                                <td id='P' ><?php echo $row['E_pan']; ?></td>
+                                <td id='S' ><?php echo $row['E_sal']; ?></td>
+                                <td id='J' ><?php echo $row['E_Jdate']; ?></td>
+                                <td id='St' ><?php if($row['E_status']) echo "Active"; ?></td>
                                 <td>
-                                    <button onclick="ajax(<?php echo $row['ID']; ?>)" class="btn btn-light">Delete</button>
-                                    <button class="btn btn-light"><a href="index.php?ID=<?php echo $row['ID']; ?>">Edit</a></button>
+                                    <button class="btn btn-light delete" data-id="<?php echo $row['ID']; ?>">Delete</button>
+                                    <button class="btn btn-light edit" data-id="<?php echo $row['ID']; ?>">Edit</button>
                                 </td>
                             </tr>
                             <?php
@@ -106,6 +106,50 @@
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+            <script>
+                $(document).ready(function(){
+                    $(".delete").click(function(){
+                        var ID=$(this).attr('data-id');
+                        $('#'+ID).remove();
+
+                        $.ajax({
+                            url: "view.php",
+                            data : {ID:ID},
+                            type : 'GET',
+                            success: function(result){
+                                $("#txt").html(result);
+                            }      
+                        });
+                    });
+
+                    $(".edit").click(function(){
+                        $(this).html('Update');
+                        var ID=$(this).attr('data-id');
+                        var N=$('#'+ID+' #N').text();
+                        var M=$('#'+ID+' #M').text();
+                        var E=$('#'+ID+' #E').text();
+                        var P=$('#'+ID+' #P').text();
+                        var S=$('#'+ID+' #S').text();
+                        var J=$('#'+ID+' #J').text();
+                        var St=$('#'+ID+' #St').text();
+                        $('#'+ID).remove();
+                        $('tbody').prepend("<tr id='"+ID+"'><td><input type='text' value='"+ID+"'></td><td><input type='text' value='"+N+"'></td><td><input type='text' value='"+M+"'></td> <td><input type='text' value='"+E+"'></td> <td><input type='text' value='"+P+"'></td> <td><input type='text' value='"+S+"'></td> <td><input type='text' value='"+J+"'></td> <td><input type='text' value='"+St+"'></td> <td><button class='btn btn-light update' data-id='"+ID+"' >Update</button></td></tr>");
+                    });
+
+                    $(".update").click(function(){
+                        var ID=$(this).attr('data-id');
+                        $.ajax({
+                            url: "demo.php",
+                            data : {ID:ID},
+                            type : 'GET',
+                            success: function(result){
+                                $("#txt").html(result);
+                            }      
+                        }); 
+                    });
+                });
+            </script>        
         </div>
     </body>
 </html>
